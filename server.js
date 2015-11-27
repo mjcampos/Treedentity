852c9fb03ea1d4eb959fe8f828a86b675f6c7475
Michael Campos <camposmj89@gmail.com>
2015-11-26 21:39:34 -0800

    Updated the navbar, body, and footer to make it dynamic.

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var app = express();
 app.use(express.static(path.join(__dirname, "./public")));
 app.use(bodyParser.json());
 
-app.listen(8888, function() {
+app.listen(8000, function() {
 	console.log("Treedentity program active");
 });
