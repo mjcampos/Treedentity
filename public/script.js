1025794f67494a726a0f7ffc5c554b1976476190
Michael Campos <camposmj89@gmail.com>
2015-11-28 08:49:58 -0800

    Fixed the problem and now on a roll to convert all the pages into a single page application. Once that is complete I need to start thinking of ways to improve the features.

--- public/script.js
+++ public/script.js
@@ -6,5 +6,10 @@ myApp.config(function($routeProvider) {
 	.when('/', {templateUrl: 'partials/home.html'})
 	.when('/page1', {templateUrl: 'partials/page1.html'})
 	.when('/page2', {templateUrl: 'partials/page2.html'})
+	.when('/page3', {templateUrl: 'partials/page3.html'})
+	.when('/page4', {templateUrl: 'partials/page4.html'})
+	.when('/page5', {templateUrl: 'partials/page5.html'})
+	.when('/page6', {templateUrl: 'partials/page6.html'})
+	.when('/page7', {templateUrl: 'partials/page7.html'})
 	.otherwise({redirectTo: '/'});
 });
